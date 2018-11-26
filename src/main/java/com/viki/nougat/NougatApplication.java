3c486bd4ace95c7982225d24d82d1700ea429e6d
Viki Chen <viki@chinadep.com>
2018-11-26 12:37:00 +0800

    数据结构

---
+++ src/main/java/com/viki/nougat/NougatApplication.java
@@ -0,0 +1,12 @@
+package com.viki.nougat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NougatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NougatApplication.class, args);
+    }
+}
